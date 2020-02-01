4c1f417ff1259a35468cfc872febacce5db8cbc8
Reece Bryant <reececb@gmail.com>
2020-02-01 11:42:09 -0600

    updated javascript file

--- mainscript.js
+++ mainscript.js
@@ -20,7 +20,7 @@ $(function(){
     //When the "check" button is clicked, it will toggle a class that will display a line through the item on the list.
     $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
       event.preventDefault();
-       $(this).closest('li').find(".shopping-item").toggleClass('.shopping-item__checked');
+       $(this).closest('li').find(".shopping-item").toggleClass('shopping-item__checked');
     });
 //When the "delete" button is clicked, it will remove the list item from our unordered list.
     $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
